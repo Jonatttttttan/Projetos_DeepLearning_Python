9418c4829059a0dfd3b51c821f13913b9749847b
jonatttttttan <jonathan.iuliany2@gmail.com>
2026-05-08 00:48:48 -0300

    criada função que recebe caminho de imagem e retorna animal

---
+++ Reconhece_animais_por_foto.py
@@ -0,0 +1,30 @@
+
+
+def reconhece_animais(caminho:str) -> str:
+    import tensorflow as tf
+    from tensorflow.keras.preprocessing import image
+    import numpy as np
+    import pandas as pd
+    import os
+
+    model_k = tf.keras.models.load_model('modelh5.keras')
+
+    img = image.load_img(caminho, target_size=(180, 180))
+    img_array = image.img_to_array(img)
+
+    # normalização
+
+    img_array = np.expand_dims(img_array, axis=0)
+
+    pred = model_k.predict(img_array)
+
+    class_names = os.listdir("C:\\Users\\Nitro\\PyCharmMiscProject\\Projetos_DeepLearning\\animais_dataset\\treino")
+    pred_class = class_names[np.argmax(pred)]
+    confidence = np.max(pred)
+
+    return f"Classe: {pred_class}, Confidence: {confidence:.2%}"
+
+if __name__ == "__main__":
+    animal = reconhece_animais("C:\\Users\\Nitro\\Downloads\\sh.png")
+    print(animal)
+
