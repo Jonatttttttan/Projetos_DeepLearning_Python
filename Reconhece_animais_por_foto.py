136f30e8316470e2445cf77912cef25aa966b276
jonatttttttan <jonathan.iuliany2@gmail.com>
2026-07-20 23:35:38 -0300

    Iniciando ML de plantas brasileiras

--- Reconhece_animais_por_foto.py
+++ Reconhece_animais_por_foto.py
@@ -25,6 +25,6 @@ def reconhece_animais(caminho:str) -> str:
     return f"Classe: {pred_class}, Confidence: {confidence:.2%}"
 
 if __name__ == "__main__":
-    animal = reconhece_animais("C:\\Users\\Nitro\\Downloads\\Captura de tela 2026-05-09 231337.png")
+    animal = reconhece_animais("C:\\Users\\Nitro\\Downloads\\Captura de tela 2026-05-20 205608.png")
     print(animal)
 
